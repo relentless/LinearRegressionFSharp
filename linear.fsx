29d9842ca77afc405c63a5ff504ba94fdd84bd3e
Grant Crofton <grantac@hotmail.com>
2014-06-28 16:20:28 +0100

    Plotted cost

--- linear.fsx
+++ linear.fsx
@@ -9,15 +9,20 @@ let data =
     File.ReadAllLines(__SOURCE_DIRECTORY__ + """\transistors.csv""").[1..] 
     |> Array.map (fun line -> 
         let values = line.Split(',')
-        (int values.[1], int64 values.[0]))
+        (int values.[1]-1970, int64 values.[0]/1000L))
 
-Chart.Point(data,Title="Transistor Count by Year", XTitle="Year", YTitle="Transistors")
+//Chart.Point(data,Title="Transistor Count by Year (000's)", XTitle="Years since 1970", YTitle="Transistors")
 
+// functions for getting values from Theta
 let intercept (x,_) = x
 let slope (_,x) = x
 
+// functions for getting values from the x,y values of the data points
+let year (x,_) = x
+let transistorCount (_,x) = x
+
 let prediction theta x =
-    (theta |> intercept) + x * (theta |> slope)
+    (theta |> intercept) + (float x) * (theta |> slope)
 
 // generates the line as given by the parameters theta (y-intercept, slope) with x ranging from 0 to x_size
 let line theta x_size =
@@ -30,9 +35,17 @@ let line theta x_size =
 // calculates the cost of the parameters theta (y-intercept, slope) in terms of mean squared distance from the data points
 let cost theta =
     data
-    |> Array.fold (fun agg el -> 
-        let predicted = prediction theta (fst el)
-        let difference = abs (int64 predicted - snd el)
-        agg + difference) 0L // TODO: square (might overflow)
+    |> Array.fold (fun total dataPoint -> 
+        let predicted = prediction theta (dataPoint|> year)
+        let difference = abs (int64 predicted - (dataPoint |> transistorCount))
+        total + (difference * difference)) 0L
+    |> (fun total -> total / int64 data.Length)
+    
+// find costs for various different slopes (keeping the y-intercept at 0 to make it easier to plot)
+let costs = 
+    [1000.0..1000.0..50000.0]
+    |> List.map (fun x -> x, cost(0.0,x))
 
-cost (0,1)
+let minCost = costs |> List.minBy snd |> snd
+
+Chart.Line(costs, Title="Cost of single-variable Theta", XTitle="Slope", YTitle="Cost").WithYAxis(Min=float minCost)
