d695da4ff0dd4bcfa470bab71c7c2672d698550d
Grant Crofton <grantac@hotmail.com>
2014-07-16 09:01:01 +0100

    continued work on multi variable

--- multi variable.fsx
+++ multi variable.fsx
@@ -36,21 +36,19 @@ let cost theta =
 
 // Create an array for parameters theta.  The first will always be 0, which we use to make
 // calculations more consistent
-let createTheta() = 
+let blankTheta = 
     let theta = Array.create featureCount 0.0
-    theta.[0] = 1.0
+    theta.[0] = 1.0 |> ignore
+    theta
 
 // minimise cost using gradient descent search
 let gradientDescent learningRate maxIterations =
 
-// TODO : finish
-    let derivativeTheta theta =
-        theta
-        |> Array.map (fun parameter ->
-            Array.zip data answers
-            |> Array.sumBy (fun (dataRecord,answer) ->
-                (prediction theta dataRecord) - answer)
-
+// TODO : finish?
+    let derivativeTheta theta parameter =
+        Array.zip data answers
+        |> Array.sumBy (fun (dataRecord,answer) ->
+            ((prediction theta dataRecord) - answer) * parameter)
 
     let rec descentSearch theta iteration = 
         match iteration < maxIterations with
@@ -59,28 +57,21 @@ let gradientDescent learningRate maxIterations =
             //printfn "iteration: %i, Theta:%A, cost:%f" iteration theta (cost theta)
             let newTheta = 
                 theta
-                |> Array.map (fun parameter -> parameter - learningRate * derivativeTheta theta)
+                |> Array.map (fun parameter -> 
+                    parameter - learningRate * derivativeTheta theta parameter)
 
             descentSearch newTheta (iteration+1)
 
-    descentSearch createTheta() 0
+    descentSearch blankTheta 0
     
 // check gradient descent is working
 let gradCosts = 
     [1..10]
-    |> List.map (fun x -> x, cost(gradientDescent 0.00001 x))
+    |> List.map (fun x -> x, cost(gradientDescent 0.01 x))
 
 Chart.Line(gradCosts, Title="Cost of Theta given by Gradient Descent", XTitle="Iterations", YTitle="Cost")
 
 // calculate the parameters theta
 let finalTheta = gradientDescent 0.00001 10
-
-// see our line against the data
-let maxBody = data |> Array.maxBy fst |> fst
-
-Chart.Combine [
-    Chart.Point(data,Title="Mammal Body vs Brain Weight", XTitle="Body Weight (Kg)", YTitle="Brain Weight (g)")
-    Chart.Line([0.0,prediction finalTheta 0.0; maxBody, prediction finalTheta maxBody])
-    ]
     
-printfn "Prediction of brain size for a mammal with a body weighing 2000kg: %fg" (prediction finalTheta 2000.0)
+printfn "Prediction of brain size for a mammal with a body weighing 2000kg: %fg" (prediction finalTheta [|2000.0;10.0|])
